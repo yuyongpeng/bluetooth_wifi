e0798d6a60d5fee184806099834f7034e4ca14e6
yuyongpeng <yuyongpeng@modernmedia.com.cn>
2018-05-14 15:57:34 +0800

    给默认的key和iv

--- main.js
+++ main.js
@@ -20,6 +20,8 @@ bleno.on('advertisingStart', function(error) {
     console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
     // dphotos.key = aes.key();
     // dphotos.iv = aes.iv();
+    dphotos.key = 'ExchangePasswordPasswordExchange';
+    dphotos.iv = 'f21dab5f0aca29c6';
     if (!error) {
         bleno.setServices([primaryService], function(error){
             console.log('set DphotosService: '  + (error ? 'error ' + error : 'success'));
