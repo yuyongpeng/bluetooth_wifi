ead8452aaa6e28e93d4291369e269e810df6aa35
yuyongpeng <yuyongpeng@modernmedia.com.cn>
2018-05-14 12:57:23 +0800

    4.0 没有crypto.randomFill 函数

--- utils.js
+++ utils.js
@@ -4,23 +4,41 @@
 var crypto = require('crypto');
 var aesutil = module.exports = {};
 
+
+function randomWord(randomFlag, min, max){
+    var str = "",
+        range = min,
+        arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+    // 随机产生
+    if(randomFlag){
+        range = Math.round(Math.random() * (max-min)) + min;
+    }
+    for(var i=0; i<range; i++){
+        pos = Math.round(Math.random() * (arr.length-1));
+        str += arr[pos];
+    }
+    return str;
+}
+
 aesutil.key = function () {
-    const buf = Buffer.alloc(16);
-    crypto.randomFillSync(buf,0,16, (err, buf) => {
-        if (err) throw err;
-        // console.log(buf.toString('hex'));
-    });
-    return buf.toString('hex');
-    // return x;
+    // const buf = Buffer.alloc(16);
+    // crypto.randomFillSync(buf,0,16, (err, buf) => {
+    //     if (err) throw err;
+    //     // console.log(buf.toString('hex'));
+    // });
+    // return buf.toString('hex');
+    return randomWord(1, 32, 32)
 }
 
 aesutil.iv = function () {
-    const buf = Buffer.alloc(8);
-    crypto.randomFillSync(buf, 0, 8, (err, buf) => {
-        if (err) throw err;
-        // console.log(buf.toString('hex'));
-    });
-    return buf.toString('hex');
+    // const buf = Buffer.alloc(8);
+    // crypto.randomFillSync(buf, 0, 8, (err, buf) => {
+    //     if (err) throw err;
+    //     // console.log(buf.toString('hex'));
+    // });
+    // return buf.toString('hex');
+    return randomWord(1, 16, 16)
 }
 
 /**
